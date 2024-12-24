0fb0d8e22ad51c557f0eb6c70eb00d7274563f44
MoisturizedDragon <126834676+MoisturizedDragon@users.noreply.github.com>
2024-12-24 16:50:47 -0500

    added a pretty_print_dict function to make the output look cleaner

--- main.py
+++ main.py
@@ -11,14 +11,18 @@ def count_characters(input):
         else:
             character_dict[character] = 0 
     return character_dict
-        
 
+def pretty_print_dict(input):
+    for key in input:
+        value = input[key]
+        print(f"The character {key} appeared {value} times.")
+        
 def main():
     file_path = "books/frankenstein.txt"
     with open(file_path) as f:
         file_contents = f.read()
 
     print(f"There are {words_in_text(file_contents)} words in this document.") 
-    print(f"Number of times each character appears in the document: {count_characters(file_contents)}") 
+    pretty_print_dict(count_characters(file_contents))
 
 main()

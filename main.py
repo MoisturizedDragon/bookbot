33719b40bbb5f71389f950cb4923330ae5f0598f
MoisturizedDragon <126834676+MoisturizedDragon@users.noreply.github.com>
2024-12-24 18:33:56 -0500

    removed the pretty_print_dict function and added a print_report function.

--- main.py
+++ main.py
@@ -6,23 +6,26 @@ def count_characters(input):
     lowercase_string = input.lower()
     character_dict = {}
     for character in lowercase_string:
-        if character in character_dict:
+        if character in character_dict and character.isalpha():
             character_dict[character] += 1
-        else:
-            character_dict[character] = 0 
+        elif character not in character_dict and character.isalpha():
+            character_dict[character] = 1 
     return character_dict
 
-def pretty_print_dict(input):
-    for key in input:
-        value = input[key]
-        print(f"The character {key} appeared {value} times.")
+def print_report(file_path, input):
+    print(f"--- Begin report of {file_path} ---")
+    print(f"There are {words_in_text(input)} words in this text.")
+    char_dict = count_characters(input)
+    sorted_char_list = sorted(char_dict.items(), reverse=True, key=lambda item: item[1])
+    for char, count in sorted_char_list:
+        print(f"The '{char}' character was found {count} times.")
+    print("--- End report ---")
         
 def main():
     file_path = "books/frankenstein.txt"
     with open(file_path) as f:
         file_contents = f.read()
 
-    print(f"There are {words_in_text(file_contents)} words in this document.") 
-    pretty_print_dict(count_characters(file_contents))
+    print_report(file_path, file_contents)
 
 main()
